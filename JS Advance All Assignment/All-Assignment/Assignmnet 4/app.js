c00351172337235e043704750578cc39cdf28ec1
Muhammad-Malaik <muhammadmalaik76@gmail.com>
2024-12-07 16:54:42 +0500

    Add files via upload

---
+++ JS Advance All Assignment/All-Assignment/Assignmnet 4/app.js
@@ -0,0 +1,47 @@
+// const arr = [
+//     42,
+//     true,
+//     "Hello, World!",
+//     {
+//         name: "John"    ,
+//         age: 25,
+//         isStudent: false
+//     },
+//     false,
+//     3.14,
+//     "JavaScript is Fun!"
+// ];
+
+
+// let stringArr = arr.map(p => p.toString()) // First
+// console.log(stringArr);
+
+// let filterMethod = arr.filter(p => typeof p === 'number') // Secound
+// console.log(filterMethod);
+
+// let forEachmethod = arr.forEach(element => { console.log(typeof element)}) // Third
+// console.log(forEachmethod);
+
+// const sumOfNumbers = arr.reduce((sum, item) => {  // Fourth
+//         if (typeof item === "number") {
+//                 return sum + item;
+//             }
+//             return sum;
+//         }, 0)
+// console.log(sumOfNumbers);
+
+// let findMethod = arr.find(p => typeof p === 'boolean') // Fifth
+// console.log(findMethod);
+
+// let findIndex = arr.findIndex((p) => typeof p === 'object')// sixth
+// console.log(findIndex);
+
+
+// let someMethod = arr.some((ele) => typeof ele === 'number') // seventh
+// console.log(someMethod);
+
+// const everyMethod = arr.every(item => typeof item === "string"); // eights
+
+// console.log( everyMethod);
+
+
